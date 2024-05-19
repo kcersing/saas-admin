377e23cd95e18bb3a3690fea9bece43cc44ee59c
kcersing <wt4@live.cn>
2024-05-20 01:38:14 +0800

    菜单

--- src/pages/components/staffls/index.tsx
+++ src/pages/components/staffls/index.tsx
@@ -1,8 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Form, Input, Select, Space } from '@arco-design/web-react';
+import { Button, Form, Input, Select, Space ,Alert} from '@arco-design/web-react';
 import { IconDelete, IconStar } from '@arco-design/web-react/icon';
 import sysService from '@/api/sys';
-
+import styles from './index.module.css';
 function Staffs() {
   const [list, setList] = useState([]);
   useEffect(() => {
@@ -13,40 +13,49 @@ function Staffs() {
   function listData() {
     sysService.staffList()
       .then((res) => {
-        if (res.data.length>0){
-          setList(res.data);
-        }
+        setList(res.data);
       });
   }
 
   const Option = Select.Option;
   const options = [10,20,30,40,50,60,70,80,90,100];
   return (
-    <>
-      <Form.List field="staffs"        
+    <div  className={styles.div1}>
+
+      <Form.List field="staffs" 
+      
       rules={[
-              {
-                validator(v, cb) {
-                  if (v?.length < 2) {
-                    return cb('必须超过两条');
-                  }
-                  return cb();
-                },
-              },
-            ]}>
+        {
+          validator(v, cb) {
+            if (v?.length < 2) {
+              return cb(      <Alert
+                closable
+                style={{ marginBottom: 20 }}
+                type='error'
+                title='Error'
+                content='Here is an error text'
+              />);
+            }
+            return cb();
+          },
+        },
+      ]}
+
+      >
 
         {(fields, { add, remove, move }) => {
           return (
             <div>
               {fields.map((item, index) => {
                 return (
+
                   <div key={item.key}>
                     <Form.Item label='销售'>
                       <Space  style={{ width: 120 }}>
                         <Form.Item
                           field={item.field + '.id'}
                           rules={[{ required: true }]}
-                        
+                          noStyle
                         >
                           <Select
                             placeholder="选择员工"
@@ -72,7 +81,7 @@ function Staffs() {
                             ))}
                           </Select>
                         </Form.Item>
-                        <Form.Item field={item.field + '.ratio'} rules={[{ required: true }]} >
+                        <Form.Item field={item.field + '.ratio'} rules={[{ required: true }]} noStyle>
                           <Select  style={{ width: 80 }} >
                             {options.map((option, index) => (
                               <Option key={option}  value={option}>
@@ -81,7 +90,7 @@ function Staffs() {
                             ))}
                           </Select>
                         </Form.Item>
-                        {!!index &&  <Button icon={<IconDelete />} shape="circle" status="danger" onClick={() => remove(index)} />}
+                        <Button icon={<IconDelete />} shape="circle" status="danger" onClick={() => remove(index)} />
                       </Space>
                     </Form.Item>
                   </div>
@@ -93,8 +102,7 @@ function Staffs() {
                     add();
                   }}
                 >
-
-                  追加销售员工
+                  添加销售员工
                 </Button>
               </Form.Item>
             </div>
@@ -102,7 +110,7 @@ function Staffs() {
         }}
       </Form.List>
 
-    </>
+    </div>
   );
 }
 
